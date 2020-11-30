92d312cdbf636e5bb0768d9afc82e85fdba04d85
Pedro Gomez De La Garza <pedro.gomezd@ext.cemex.com>
2020-11-30 03:31:39 -0600

    Styling for tasks

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -6,7 +6,27 @@ const Task = require('../model/task');
 
 // Create task
 router.get('/new', async (req, res) => {
-
+    console.log(req.params);
+    var user = await User.findById(req.rootParams.userId);
+    var board = await Board.findById(req.params.boardId);
+    var todo = await Task.find({boardId: req.params.boardId, status: 'todo'});
+    var inprogress = await Task.find({boardId: req.params.boardId, status: 'inprogress'});
+    var done = await Task.find({boardId: req.params.boardId, status: 'done'});
+    console.log(user);
+    console.log(board);
+    if (user && board) {
+        res.render('board', {
+            user: user,
+            board: board,
+            todo: todo,
+            inprogress: inprogress,
+            done: done,
+            newTask: true
+        });
+    }
+    else {
+        res.status(404).send("404 Not found");
+    }
 });
 
 router.post('/', async (req, res) => {
